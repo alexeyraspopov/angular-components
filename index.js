29a5920d0ecb2adfac0c094907cfd0b3631213b3
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-12-06 17:57:36 +0200

    added simple routing based on ui-router

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 import App from 'App';
 import appRoot from 'components/app-root/app-root';
+
+App.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', ($stateProvider, $urlRouterProvider, $locationProvider) => {
+	$locationProvider.html5Mode({
+		enabled: true,
+		requireBase: false
+	});
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider
+		.state('home', {
+			url: '/',
+			template: '<app-root></app-root>'
+		});
+}]);
